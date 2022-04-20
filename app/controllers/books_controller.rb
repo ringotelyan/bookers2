453678c31a7ccaf4b34ad7cad54fd412cc57dd50
ringotelyan <sute.dh9999@gmail.com>
2022-04-20 16:14:17 +0000

    [Update]userのedit/update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
 end
