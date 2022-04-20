c9a3518c10a9d00132baa0e3e3c240afcb437c34
ringotelyan <sute.dh9999@gmail.com>
2022-04-20 14:35:33 +0000

    [Update]bookイジリ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_20_111710) do
+ActiveRecord::Schema.define(version: 2022_04_20_090409) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 2022_04_20_111710) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
