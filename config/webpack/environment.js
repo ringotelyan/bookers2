a123b3d6608bf261165770176398a7f7995d5810
ringotelyan <sute.dh9999@gmail.com>
2022-04-23 06:33:42 +0000

    [Update]bootstrap導入

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = rewuire('webpack')
+environment.pluhins.prepand(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
+  })
+  )
