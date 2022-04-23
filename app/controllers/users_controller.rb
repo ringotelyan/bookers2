0c66eedf4fcd1e9ddbf4483d8bb0fb6183dba939
ringotelyan <sute.dh9999@gmail.com>
2022-04-23 05:26:40 +0000

    [Update]わけわからんエラー解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:use).permit(:name, :introduction, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
   def correct_user
