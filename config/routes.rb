522b35a28614e3fea4ceb9822bce452f8cbea514
ringotelyan <sute.dh9999@gmail.com>
2022-04-24 18:29:37 +0000

    [Update]サクセスメッセージなんとかした

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 end
